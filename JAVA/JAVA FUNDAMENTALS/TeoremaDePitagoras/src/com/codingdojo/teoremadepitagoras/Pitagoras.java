d524fbb090fb2fcbf7e30b6b40158f04f07ba629
Nacho <ignacio.miranda0201@gmail.com>
2023-07-04 03:59:40 -0600

    update java teoremadepitagorasv2

--- JAVA/JAVA FUNDAMENTALS/TeoremaDePitagoras/src/com/codingdojo/teoremadepitagoras/Pitagoras.java
+++ JAVA/JAVA FUNDAMENTALS/TeoremaDePitagoras/src/com/codingdojo/teoremadepitagoras/Pitagoras.java
@@ -1,7 +1,8 @@
 package com.codingdojo.teoremadepitagoras;
 
 public class Pitagoras {
+	
     public static double calcularHipotenusa(int catetoA, int catetoB) {
-    	return Math.sqrt(Math.pow(catetoA, 2)+Math.pow(catetoB, 2));
+    	return Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
     }
 }
