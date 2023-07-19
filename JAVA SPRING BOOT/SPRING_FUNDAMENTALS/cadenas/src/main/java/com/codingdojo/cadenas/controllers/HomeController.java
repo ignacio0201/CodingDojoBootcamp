d1dca59ada1819e607d92acb20139cd1991b4c20
Ignacio Miranda <ignacio.miranda0201@gmail.com>
2023-07-18 22:48:48 -0400

    message

---
+++ JAVA SPRING BOOT/SPRING_FUNDAMENTALS/cadenas/src/main/java/com/codingdojo/cadenas/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.codingdojo.cadenas.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+    @RequestMapping("/")
+    public String hello() {
+            return "Hello client! How are you doing?";
+    }
+    
+    @RequestMapping("/random")
+    public String message() {
+		return "Spring Boot is great! So easy to just respond with strings";
+    }
+}
